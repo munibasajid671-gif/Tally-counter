143f5857b17ff0bea0a6f4ae5d04f0683977119f
munibasajid671-gif <munibasajid671@gmail.com>
2026-04-25 21:14:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let count = 0
+function increase(){
+    count ++;
+    document.getElementById ('counter-value').innerHTML = count
+}
+function decrease(){
+    count -- ;
+    document.getElementById ('counter-value').innerHTML = count
+    
+}
+function reset(){
+    count = 0;
+    document.getElementById ('counter-value').innerHTML = count
+}
